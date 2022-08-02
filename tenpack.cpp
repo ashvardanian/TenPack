76ce55f1e9831389937ecf0f19addf3bb8d53bd7
Ashot Vardanian <ashvardanian@gmail.com>
2022-08-02 13:32:45 +0400

    Add: TurboJpeg external Cmake project

--- tenpack.cpp
+++ tenpack.cpp
@@ -1,16 +1,31 @@
+#include <turbojpeg.h>
 
-constexpr char prefix_jpeg_k[3] {0xFF, 0xD8, 0xFF};
-constexpr char prefix_jpeg2000_k[13] {0x0, 0x0, 0x0, 0xC, 0x6A, 0x50, 0x20, 0x20, 0xD, 0xA, 0x87, 0xA, 0x0};
-constexpr char prefix_png_k[4] {0x89, 0x50, 0x4E, 0x47};
-constexpr char prefix_gif_k[3] {0x47, 0x49, 0x46};
-constexpr char prefix_bmp_k[2] {0x42, 0x4D};
-constexpr char prefix_jxr_k[3] {0x49, 0x49, 0xBC};
-constexpr char prefix_psd_k[4] {0x38, 0x42, 0x50, 0x53};
-constexpr char prefix_ico_k[4] {0x00, 0x00, 0x01, 0x00};
-constexpr char prefix_dwg_k[4] {0x41, 0x43, 0x31, 0x30};
+#include "tenpack.h"
+
+constexpr unsigned char prefix_jpeg_k[3] {0xFF, 0xD8, 0xFF};
+constexpr unsigned char prefix_jpeg2000_k[13] {0x0, 0x0, 0x0, 0xC, 0x6A, 0x50, 0x20, 0x20, 0xD, 0xA, 0x87, 0xA, 0x0};
+constexpr unsigned char prefix_png_k[4] {0x89, 0x50, 0x4E, 0x47};
+constexpr unsigned char prefix_gif_k[3] {0x47, 0x49, 0x46};
+constexpr unsigned char prefix_bmp_k[2] {0x42, 0x4D};
+constexpr unsigned char prefix_jxr_k[3] {0x49, 0x49, 0xBC};
+constexpr unsigned char prefix_psd_k[4] {0x38, 0x42, 0x50, 0x53};
+constexpr unsigned char prefix_ico_k[4] {0x00, 0x00, 0x01, 0x00};
+constexpr unsigned char prefix_dwg_k[4] {0x41, 0x43, 0x31, 0x30};
 
 // Relevant OpenCV funcs:
 // https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html#ga5a0acefe5cbe0a81e904e452ec7ca733
 // https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html#ga26a67788faa58ade337f8d28ba0eb19e
 // https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
 // https://docs.opencv.org/4.x/d0/d61/group__cudacodec.html
+
+void tenpack_guess_dimensions( //
+    void* content_bytes,
+    size_t content_length,
+    tenpack_format_t format,
+    size_t* guessed_dimensions) {
+
+    // There is some documentation in libjpeg.txt. libjpeg is a very low-level, steep-learning-curve,
+    // old school c library. To use it effectively you need to be familiar with setjmp and longjmp,
+    // c structure layouts, function pointers, and lots of other low-level C stuff. It's a bear to
+    // work with but possible to do a great deal with minimal resource usage.
+}
--- tenpack.h
+++ tenpack.h
@@ -1,5 +1,23 @@
-
+/**
+ * @file tenpack.h
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-08-02
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 enum tenpack_format_t {
     tenpack_format_unknown_k,
@@ -22,6 +40,10 @@ enum tenpack_format_t {
 
 void tenpack_guess_format(void*, size_t, tenpack_format_t*);
 
-void tenpack_guess_dimensions(void*, size_t, tenpack_format_t, size_t);
+void tenpack_guess_dimensions(void*, size_t, tenpack_format_t, size_t*);
 
 void tenpack_upack(void*, size_t, tenpack_format_t, size_t* slice, void* output_begin, size_t output_stride);
+
+#ifdef __cplusplus
+} /* end extern "C" */
+#endif
