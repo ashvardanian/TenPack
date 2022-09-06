486e704becec7c1daf087a611bbfd66e3ea2c101
Arman-Ghazaryan <arman.ghazaryan.0517@gmail.com>
2022-09-06 14:56:12 +0400

    change test solution

--- test.cpp
+++ test.cpp
@@ -4,48 +4,46 @@
 
 #include "tenpack.h"
 
-bool decode(std::vector<std::string> const& paths, std::vector<std::vector<u_int8_t>>& unpacked_buffers) {
-    unpacked_buffers.resize(paths.size());
-    size_t output_size = 0;
-    size_t idx = 0;
+bool decode(std::string const& path, std::vector<uint8_t>& unpacked, tenpack_ctx_t ctx, tenpack_dimensions_t& dims) {
 
-    for (auto const& path : paths) {
+    // Read entire file
+    std::ifstream file(path, std::ifstream::binary);
+    std::vector<uint8_t> input((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
 
-        // Read entire file
-        std::ifstream file(path, std::ifstream::binary);
-        std::vector<u_int8_t> input((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        file.close();
+    // 1
+    tenpack_format_t format;
+    if (!tenpack_guess_format(input.data(), input.size(), &format, &ctx))
+        throw std::invalid_argument("Can't guess file format:" + path);
 
-        // We are going to need temporary memory
-        tenpack_ctx_t ctx = nullptr;
+    // 2
+    if (!tenpack_guess_dimensions(input.data(), input.size(), format, &dims, &ctx))
+        throw std::invalid_argument("Can't guess dimensions:" + path);
 
-        // 1
-        tenpack_format_t format;
-        if (!tenpack_guess_format(input.data(), input.size(), &format, &ctx))
-            throw std::invalid_argument("Can't guess file format:" + path);
-
-        // 2
-        tenpack_dimensions_t dims;
-        if (!tenpack_guess_dimensions(input.data(), input.size(), format, &dims, &ctx))
-            throw std::invalid_argument("Can't guess dimensions:" + path);
-
-        // 3
-        std::vector<u_int8_t> unpacked(dims.width * dims.height * dims.frames * dims.channels * dims.bytes_per_channel);
-        output_size = unpacked.size();
-        if (!tenpack_unpack(input.data(), input.size(), format, &dims, unpacked.data(), &ctx))
-            throw std::invalid_argument("Can't export:" + path);
-
-        unpacked_buffers[idx].resize(output_size);
-        std::memcpy(unpacked_buffers[idx++].data(), unpacked.data(), output_size);
-        tenpack_context_free(ctx);
-    }
+    // 3
+    unpacked.resize(dims.width * dims.height * dims.frames * dims.channels * dims.bytes_per_channel);
+    if (!tenpack_unpack(input.data(), input.size(), format, &dims, unpacked.data(), &ctx))
+        throw std::invalid_argument("Can't export:" + path);
 
     return true;
 }
 
-bool check_output_equality(std::vector<std::vector<u_int8_t>>& unpacked_buffers) {
+bool check_output_equality(std::vector<std::string> const& paths) {
 
+    std::vector<std::vector<uint8_t>> unpacked_buffers(paths.size());
+    std::vector<uint8_t> unpacked;
+
+    size_t idx = 0;
     bool success = false;
+    bool success_decode = false;
+
+    // We are going to need temporary memory
+    tenpack_ctx_t ctx = nullptr;
+
+    tenpack_dimensions_t dims;
+
+    for (auto const& path : paths)
+        success_decode = decode(path, unpacked, ctx, dims);
 
     for (size_t idx = 0; idx < unpacked_buffers.size() - 1; ++idx)
         success = std::memcmp( //
@@ -53,6 +51,7 @@ bool check_output_equality(std::vector<std::vector<u_int8_t>>& unpacked_buffers)
                       unpacked_buffers[idx + 1].data(),
                       unpacked_buffers[idx].size()) == false;
 
+    tenpack_context_free(ctx);
     return success;
 }
 
@@ -66,13 +65,8 @@ int main(int argc, char* argv[]) {
                                           "assets/jpeg_black.jpg",
                                           "assets/png_black.png"};
 
-    std::vector<std::vector<u_int8_t>> unpacked_buffers;
-    
-    decode(paths_white, unpacked_buffers);
-    bool success_white = check_output_equality(unpacked_buffers);
-
-    decode(paths_black, unpacked_buffers);
-    bool success_black = check_output_equality(unpacked_buffers);
+    bool success_white = check_output_equality(paths_white);
+    bool success_black = check_output_equality(paths_black);
 
     return 0;
 }
