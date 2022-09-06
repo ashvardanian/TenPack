7cc84d2535b14f1a02df91465d2e7d23be72b8d0
Arman-Ghazaryan <arman.ghazaryan.0517@gmail.com>
2022-09-06 14:59:29 +0400

    change gif solution

--- tenpack.cpp
+++ tenpack.cpp
@@ -6,11 +6,18 @@
 
 #define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
-#include <vips/vips.h>
 #include <turbojpeg.h>
 #include <spng.h>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <nsgif.h>
+}
+
 #include "tenpack.h"
+
+#define BYTES_PER_PIXEL 4
+#define MAX_IMAGE_BYTES (48 * 1024 * 1024)
 
 // Image
 constexpr unsigned char prefix_jpeg_k[3] {0xFF, 0xD8, 0xFF};
@@ -32,6 +39,42 @@ constexpr unsigned char prefix_riff_k[4] {0x52, 0x49, 0x46, 0x46};
 // Audio
 constexpr unsigned char prefix_wav_k[4] {0x57, 0x41, 0x56, 0x45};
 
+static void* bitmap_create(int width, int height) {
+    /* ensure a stupidly large bitmap is not created */
+    if (((long long)width * (long long)height) > (MAX_IMAGE_BYTES / BYTES_PER_PIXEL)) {
+        return NULL;
+    }
+    return calloc(width * height, BYTES_PER_PIXEL);
+}
+
+static void bitmap_set_opaque(void* bitmap, bool opaque) {
+    (void)opaque; /* unused */
+    (void)bitmap; /* unused */
+    assert(bitmap);
+}
+
+static bool bitmap_test_opaque(void* bitmap) {
+    (void)bitmap; /* unused */
+    assert(bitmap);
+    return false;
+}
+
+static unsigned char* bitmap_get_buffer(void* bitmap) {
+    assert(bitmap);
+    return reinterpret_cast<unsigned char*>(bitmap);
+}
+
+static void bitmap_destroy(void* bitmap) {
+    assert(bitmap);
+    free(bitmap);
+}
+
+static void bitmap_modified(void* bitmap) {
+    (void)bitmap; /* unused */
+    assert(bitmap);
+    return;
+}
+
 template <typename at, std::size_t length_ak>
 bool matches(at (&prefix)[length_ak], at* content, std::size_t content_len) {
     if (content_len < length_ak) [[unlikely]]
@@ -44,8 +87,18 @@ bool matches(at (&prefix)[length_ak], at* content, std::size_t content_len) {
 }
 
 class ctx_t {
+
+    nsgif_bitmap_cb_vt bitmap_callbacks = { //
+        bitmap_create,
+        bitmap_destroy,
+        bitmap_get_buffer,
+        bitmap_set_opaque,
+        bitmap_test_opaque,
+        bitmap_modified};
+
     tjhandle jpeg_ = nullptr;
     spng_ctx* png_ = nullptr;
+    nsgif_t* gif_ = nullptr;
 
   public:
     ~ctx_t() {
@@ -53,10 +106,18 @@ class ctx_t {
             tjDestroy(jpeg_);
         if (png_)
             spng_ctx_free(png_);
+        if (gif_)
+            nsgif_destroy(gif_);
     }
 
     spng_ctx* png() { return png_ = png_ ?: spng_ctx_new(0); }
     tjhandle jpeg() { return jpeg_ = jpeg_ ?: tjInitDecompress(); }
+    nsgif_t* gif() {
+        if (!gif_) {
+            nsgif_create(&bitmap_callbacks, NSGIF_BITMAP_FMT_R8G8B8A8, &gif_);
+        }
+        return gif_;
+    };
 };
 
 size_t size_bytes(tenpack_dimensions_t const& dims) {
@@ -166,18 +227,21 @@ bool tenpack_guess_dimensions( //
     }
 
     case tenpack_format_t::tenpack_gif_k: {
-        // GIF header: https://www.libvips.org/API/current/VipsImage.html#vips-image-new-from-buffer
-        //             https://www.libvips.org/API/current/libvips-header
 
-        VipsImage* vips = nullptr;
-        vips = vips_image_new_from_buffer(data, len, "", NULL);
+        bool success = nsgif_data_scan( //
+                           ctx_ptr->gif(),
+                           len,
+                           reinterpret_cast<uint8_t*>(const_cast<void*>(data))) == 0;
 
-        dims.frames = vips_image_get_n_pages(vips);
-        dims.height = size_t(vips->Ysize);
-        dims.width = size_t(vips->Xsize);
+        auto info = nsgif_get_info(ctx_ptr->gif());
+
         dims.bytes_per_channel = 1;
-        dims.channels = vips->Bands;
-        return vips;
+        dims.channels = 4;
+        dims.width = info->width;
+        dims.height = info->height;
+        dims.frames = info->frame_count;
+
+        return success;
     }
 
     case tenpack_format_t::tenpack_wav_k: {
@@ -277,15 +341,26 @@ bool tenpack_unpack( //
     }
 
     case tenpack_format_t::tenpack_gif_k: {
-        // Decoding API: https://www.libvips.org/API/current/VipsImage.html#vips-image-write-to-memory
-        // Flags: https://www.libvips.org/API/current/VipsForeignSave.html#VipsForeignFlags
 
-        VipsImage* vips = nullptr;
-        vips = vips_image_new_from_buffer(data, len, "", NULL);
+        bool success = false;
 
-        size_t length = size_bytes(dims);
-        output = vips_image_write_to_memory(vips, &length);
-        return output;
+        bool success_parse = nsgif_data_scan( //
+                                 ctx_ptr->gif(),
+                                 len,
+                                 reinterpret_cast<uint8_t*>(const_cast<void*>(data))) == 0;
+
+        size_t size = dims.channels * dims.height * dims.width;
+        void* bitmap = nullptr;
+        
+        for (size_t frame_num = 0; frame_num < dims.frames; ++frame_num) {
+            success = nsgif_frame_decode(ctx_ptr->gif(), frame_num, &bitmap) == 0;
+            std::copy( //
+                reinterpret_cast<uint8_t*>(bitmap),
+                reinterpret_cast<uint8_t*>(bitmap) + size,
+                reinterpret_cast<uint8_t*>(output));
+        }
+
+        return success;
     }
 
     case tenpack_format_t::tenpack_wav_k: {
