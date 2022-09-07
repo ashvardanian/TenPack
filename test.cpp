a2272292aa870f5504c28f74f2f9d87fe79874a2
Arman-Ghazaryan <arman.ghazaryan.0517@gmail.com>
2022-09-07 13:06:16 +0400

    Change: test solution

--- test.cpp
+++ test.cpp
@@ -4,7 +4,12 @@
 
 #include "tenpack.h"
 
-bool decode(std::string const& path, std::vector<uint8_t>& unpacked, tenpack_ctx_t ctx, tenpack_dimensions_t& dims) {
+enum colors {
+    black = 0x00,
+    white = 0xFF,
+};
+
+void decode(std::string const& path, std::vector<uint8_t>& unpacked, tenpack_ctx_t ctx, tenpack_dimensions_t& dims) {
 
     // Read entire file
     std::ifstream file(path, std::ifstream::binary);
@@ -24,38 +29,35 @@ bool decode(std::string const& path, std::vector<uint8_t>& unpacked, tenpack_ctx
     unpacked.resize(dims.width * dims.height * dims.frames * dims.channels * dims.bytes_per_channel);
     if (!tenpack_unpack(input.data(), input.size(), format, &dims, unpacked.data(), &ctx))
         throw std::invalid_argument("Can't export:" + path);
-
-    return true;
 }
 
-bool check_output_equality(std::vector<std::string> const& paths) {
+bool check_output_equality(std::vector<uint8_t> const& buffer, colors color, std::size_t channels_count) {
+    bool success = true;
 
-    std::vector<std::vector<uint8_t>> unpacked_buffers(paths.size());
+    if (channels_count == 4) {
+        for (size_t idx = 0; idx < buffer.size(); idx += 4) {
+            if (!success)
+                return false;
+            for (size_t rgb = 0; rgb < 3; ++rgb)
+                success = buffer[idx + rgb] == color;
+        }
+        return success;
+    }
+    for (size_t idx = 0; idx < buffer.size(); ++idx) {
+        if (!success)
+            return false;
+        success = buffer[idx] == color;
+    }
+    return success;
+}
+
+int main(int argc, char* argv[]) {
     std::vector<uint8_t> unpacked;
-
-    size_t idx = 0;
-    bool success = false;
-    bool success_decode = false;
 
     // We are going to need temporary memory
     tenpack_ctx_t ctx = nullptr;
 
     tenpack_dimensions_t dims;
-
-    for (auto const& path : paths)
-        success_decode = decode(path, unpacked, ctx, dims);
-
-    for (size_t idx = 0; idx < unpacked_buffers.size() - 1; ++idx)
-        success = std::memcmp( //
-                      unpacked_buffers[idx].data(),
-                      unpacked_buffers[idx + 1].data(),
-                      unpacked_buffers[idx].size()) == false;
-
-    tenpack_context_free(ctx);
-    return success;
-}
-
-int main(int argc, char* argv[]) {
 
     std::vector<std::string> paths_white {//
                                           "assets/jpeg_white.jpg",
@@ -65,8 +67,25 @@ int main(int argc, char* argv[]) {
                                           "assets/jpeg_black.jpg",
                                           "assets/png_black.png"};
 
-    bool success_white = check_output_equality(paths_white);
-    bool success_black = check_output_equality(paths_black);
+    bool success_white = true;
+    bool success_black = true;
 
+    // White images
+    for (size_t idx = 0; idx < paths_white.size() && success_white; ++idx) {
+        decode(paths_white[idx], unpacked, ctx, dims);
+        success_white = check_output_equality(unpacked, colors::white, dims.channels);
+        unpacked.clear();
+    }
+    ///
+
+    // Black images
+    for (size_t idx = 0; idx < paths_black.size() && success_black; ++idx) {
+        decode(paths_black[idx], unpacked, ctx, dims);
+        success_black = check_output_equality(unpacked, colors::black, dims.channels);
+        unpacked.clear();
+    }
+    ///
+
+    tenpack_context_free(ctx);
     return 0;
 }
