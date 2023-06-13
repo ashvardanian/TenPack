42c27cb1ab631a1f37e22d9bc054a44aba3cbfff
Arman <91345263+Arman-Ghazaryan@users.noreply.github.com>
2023-06-13 10:42:39 +0000

    Refactor: cmake interface

--- cmake/libaudio.cmake
+++ cmake/libaudio.cmake
@@ -1,20 +1,25 @@
 include(ExternalProject)
 find_package(Git REQUIRED)
 
+set(PREFIX "${CMAKE_BINARY_DIR}/_deps")
+set(DOWNLOAD_DIR "${PREFIX}/libaudio-src")
+set(SOURCE_DIR "${PREFIX}/libaudio-src")
+
 ExternalProject_Add(
     ep-libaudio
     GIT_REPOSITORY https://github.com/mackron/miniaudio.git
+    GIT_TAG 0.11.17
 
-    PREFIX "_deps"
-    DOWNLOAD_DIR "_deps/libaudio-src"
-    SOURCE_DIR "_deps/libaudio-src"
-    STAMP_DIR "_deps/libaudio-stamp"
-    TMP_DIR "_deps/libaudio-tmp"
+    PREFIX ${PREFIX}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${SOURCE_DIR}
+    STAMP_DIR ${PREFIX}/libaudio-stamp
+    TMP_DIR ${PREFIX}/libaudio-tmp
 
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
 )
 
-set(LIBAUDIO_INCLUDE_DIR _deps/libaudio-src)
+set(LIBAUDIO_INCLUDE_DIR ${SOURCE_DIR})
 include_directories(${LIBAUDIO_INCLUDE_DIR})
--- cmake/libspng.cmake
+++ cmake/libspng.cmake
@@ -1,23 +1,28 @@
 include(ExternalProject)
 
+set(PREFIX "${CMAKE_BINARY_DIR}/_deps")
+set(DOWNLOAD_DIR "${PREFIX}/libspng-src")
+set(SOURCE_DIR "${PREFIX}/libspng-src")
+set(BINARY_DIR "${PREFIX}/libspng-build")
+
 ExternalProject_Add(
     ep-libspng
     URL https://github.com/randy408/libspng/archive/refs/tags/v0.7.2.tar.gz
 
-    PREFIX "_deps"
-    DOWNLOAD_DIR "_deps/libspng-src"
-    LOG_DIR "_deps/libspng-log"
-    STAMP_DIR "_deps/libspng-stamp"
-    TMP_DIR "_deps/libspng-tmp"
-    SOURCE_DIR "_deps/libspng-src"
-    INSTALL_DIR "_deps/libspng-install"
-    BINARY_DIR "_deps/libspng-build"
+    PREFIX "${PREFIX}"
+    DOWNLOAD_DIR "${DOWNLOAD_DIR}"
+    LOG_DIR "${PREFIX}/libspng-log"
+    STAMP_DIR "${PREFIX}/libspng-stamp"
+    TMP_DIR "${PREFIX}/libspng-tmp"
+    SOURCE_DIR "${SOURCE_DIR}"
+    INSTALL_DIR "${PREFIX}/libspng-install"
+    BINARY_DIR "${BINARY_DIR}"
 
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=clean
 )
 
-set(LIBSPNG_INCLUDE_DIR _deps/libspng-build/clean/include)
-set(LIBSPNG_LIB_DIR _deps/libspng-build/clean/lib)
+set(LIBSPNG_INCLUDE_DIR ${BINARY_DIR}/clean/include)
+set(LIBSPNG_LIB_DIR ${BINARY_DIR}/clean/lib)
 
 include_directories(${LIBSPNG_INCLUDE_DIR})
 link_directories(${LIBSPNG_LIB_DIR})
