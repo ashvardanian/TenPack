b11a5923886562c1d180b4730f7becfaea9baf3c
Arman Ghazaryan <91345263+Arman-Ghazaryan@users.noreply.github.com>
2023-08-22 18:24:51 +0400

    Add: CMake build configuration

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+import sys
+import re
+
+from os.path import dirname
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import subprocess
+
 import os
 
 from setuptools import setup
@@ -10,10 +18,52 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
+
+class CMakeExtension(Extension):
+    def __init__(self, name, source_dir=''):
+        Extension.__init__(self, name, sources=[])
+        self.source_dir = os.path.abspath(source_dir)
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extension_dir = os.path.abspath(dirname(self.get_ext_fullpath(ext.name)))
+
+        if not extension_dir.endswith(os.path.sep):
+            extension_dir += os.path.sep
+
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extension_dir}",
+            f"-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY={extension_dir}",
+            '-DCMAKE_BUILD_TYPE=Release',
+            '-DTENPACK_BUILD_PYTHON=1',
+        ]
+
+        if 'CMAKE_ARGS' in os.environ:
+            cmake_args += [item for item in os.environ['CMAKE_ARGS'].split(' ') if item]
+
+        if sys.platform.startswith('darwin'):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r'-arch (\S+)', os.environ.get('ARCHFLAGS', ''))
+            if archs:
+                cmake_args += ['-DCMAKE_OSX_ARCHITECTURES={}'.format(';'.join(archs))]
+
+        build_args = []
+        if sys.platform.startswith('win32'):
+            build_args += ['--config', 'Release']
+
+        subprocess.check_call(['cmake', ext.source_dir] + cmake_args)
+        print('\nName: ', ext.name, '\n')
+        print('\Source dir: ', ext.source_dir, '\n')
+        subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args)
+
+    def run(self):
+        build_ext.run(self)
+
+
 setup(
     name=__libname__,
     version=__version__,
-
     author='Ashot Vardanian',
     author_email='info@unum.cloud',
     url='https://github.com/ashvardanian/TenPack',
@@ -36,5 +86,7 @@ setup(
     ],
     py_modules=['tenpack_module'],
     extras_require={'test': 'pytest'},
+    ext_modules=[CMakeExtension('tenpack_module')],
+    cmdclass={'build_ext': CMakeBuild},
     python_requires='>=3.6',
 )
